395c5c1545a4ef5270bd9dfa353266eaf54449b0
Gustavo Niemeyer <gustavo@niemeyer.net>
2013-11-14 11:43:06 -0200

    Add reparent example.

---
+++ examples/reparent/rect.qml
@@ -0,0 +1,10 @@
+import QtQuick 2.0
+
+Rectangle {
+	//x: 100
+	//y: 100
+	anchors.centerIn: parent
+	width: 50
+	height: 50
+	color: "red"
+}
---
+++ examples/reparent/reparent.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/niemeyer/qml"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	qml.Init(nil)
+
+	engine := qml.NewEngine()
+
+	base, err := engine.LoadFile("base.qml")
+	if err != nil {
+		return err
+	}
+	rect, err := engine.LoadFile("rect.qml")
+	if err != nil {
+		return err
+	}
+
+	win := base.CreateWindow(nil)
+	obj := rect.Create(nil)
+
+	obj.Set("parent", win.Root())
+
+	win.Show()
+	win.Wait()
+
+	return nil
+}
