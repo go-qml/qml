398316ae5d370059c3e6b7e9dd56274379c8dfa0
Valentin-Costel Hăloiu <vially.ichb@gmail.com>
2014-01-12 21:40:54 +0200

    Add more include paths

--- bridge.go
+++ bridge.go
@@ -4,7 +4,9 @@ package qml
 // #cgo CPPFLAGS: -I/usr/include/qt5/QtCore/5.0.2/QtCore
 // #cgo CPPFLAGS: -I/usr/include/qt5/QtCore/5.1.1/QtCore
 // #cgo CPPFLAGS: -I/usr/include/qt5/QtCore/5.2.0/QtCore
+// #cgo CPPFLAGS: -I/usr/include/qt/QtCore/5.0.2/QtCore
 // #cgo CPPFLAGS: -I/usr/include/qt/QtCore/5.1.1/QtCore
+// #cgo CPPFLAGS: -I/usr/include/qt/QtCore/5.2.0/QtCore
 // #cgo CXXFLAGS: -std=c++0x -pedantic-errors -Wall -fno-strict-aliasing
 // #cgo LDFLAGS: -lstdc++
 // #cgo pkg-config: Qt5Core Qt5Widgets Qt5Quick glib-2.0
