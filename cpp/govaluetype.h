57b07addbe87ec9b45b7ab7c4ac6e144ceb31cfd
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-01-21 16:26:39 -0200

    Added GoPaintedValueType and registration funcs.

--- cpp/govaluetype.h
+++ cpp/govaluetype.h
@@ -23,6 +23,26 @@ public:
     static QMetaObject staticMetaObject;
 };
 
+template <int N>
+class GoPaintedValueType : public GoPaintedValue
+{
+public:
+
+    GoPaintedValueType()
+        : GoPaintedValue(hookGoValueTypeNew(this, typeSpec), typeInfo, 0) {};
+
+    static void init(GoTypeInfo *info, GoTypeSpec_ *spec)
+    {
+        typeInfo = info;
+        typeSpec = spec;
+        static_cast<QMetaObject &>(staticMetaObject) = *metaObjectFor(typeInfo);
+    };
+
+    static GoTypeSpec_ *typeSpec;
+    static GoTypeInfo *typeInfo;
+    static QMetaObject staticMetaObject;
+};
+
 #endif // GOVALUETYPE_H
 
 // vim:ts=4:sw=4:et
