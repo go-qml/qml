c1f113f17c98335e08768ac75e8181af3d980b6e
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-02-05 02:35:50 -0200

    Drop requirement on GL extensions for now.
    Will be back later in a more portable form.

--- gl/gl.go
+++ gl/gl.go
@@ -6,20 +6,6 @@ package gl
 // #cgo  darwin LDFLAGS: -framework OpenGL
 // #cgo pkg-config: Qt5Core Qt5Widgets Qt5Quick Qt5OpenGL
 //
-// #define GL_GLEXT_PROTOTYPES
 // #include "gl.h"
-// #include "glext.h"
 //
 import "C"
-
-import (
-	"fmt"
-	"unsafe"
-)
-
-func GenBuffers(n Sizei, buffers []Uint) {
-	if Sizei(len(buffers)) < n {
-		panic(fmt.Sprintf("trying to use %d elements in a buffer of length %d", n, len(buffers)))
-	}
-	C.glGenBuffers(C.GLsizei(n), (*C.GLuint)(unsafe.Pointer(&buffers[0])))
-}
