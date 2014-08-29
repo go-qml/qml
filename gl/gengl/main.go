9031cc66eced43ca252edba1d179cba0a22807f5
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-08-28 21:42:05 -0300

    Drop explicit linking with GL libraries.

--- gl/gengl/main.go
+++ gl/gengl/main.go
@@ -908,8 +908,6 @@ package GL
 
 // #cgo CXXFLAGS: -std=c++0x -pedantic-errors -Wall -fno-strict-aliasing 
 // #cgo LDFLAGS: -lstdc++
-// #cgo !darwin LDFLAGS: -lGL
-// #cgo  darwin LDFLAGS: -framework OpenGL
 // #cgo pkg-config: Qt5Core Qt5OpenGL
 //
 // #include "funcs.h"
