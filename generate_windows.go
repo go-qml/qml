5208aa3609570db1c773b474307df06f8fb6051f
chai2010 <chaishushan@gmail.com>
2014-09-10 17:50:46 +0800

    support go generate

---
+++ generate_windows.go
@@ -0,0 +1,7 @@
+// Copyright 2014 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate cmd /c call cpp\build_msvc.bat
+
+package qml
