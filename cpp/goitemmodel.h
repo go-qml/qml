45d4715a27ab22e7cc73ffd86bdaa67e50f37624
Matt Peterson <matt.peterson@getweave.com>
2016-07-12 11:17:59 -0600

    Don't use newer C++ features

--- cpp/goitemmodel.h
+++ cpp/goitemmodel.h
@@ -20,7 +20,7 @@ public:
 
 	// Required functions
 	int 			columnCount(const QModelIndex &parent = QModelIndex()) const;
-	QVariant 		data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+	QVariant 		data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 	QModelIndex 	index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
 	QModelIndex 	parent(const QModelIndex &index) const;
 	int 			rowCount(const QModelIndex &parent = QModelIndex()) const;
