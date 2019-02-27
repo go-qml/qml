1a4a2513d8a6330eb4fb5699bc3ade778f14a99d
neclepsio <neclepsio@gmail.com>
2019-02-27 13:43:23 +0100

    Added support for Go 1.12

--- cdata/cdata14_amd64.s
+++ cdata/cdata14_amd64.s
@@ -2,12 +2,14 @@
 
 #include "textflag.h"
 
+/*
 TEXT ·Ref(SB),NOSPLIT,$8-8
 	CALL runtime·acquirem(SB)
 	MOVQ 0(SP), AX
 	MOVQ AX, ret+0(FP)
 	CALL runtime·releasem(SB)
 	RET
+*/
 
 TEXT ·Addrs(SB),NOSPLIT,$0-16
 	MOVQ	$runtime·main(SB), AX
